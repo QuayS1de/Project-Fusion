68631a93ac3219ff015ee3b2801f88ac8a13343d
QuaySide-CP <axel235@mail.ru>
2021-06-20 15:09:18 +0300

    code for sliders

---
+++ index.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function () {
+    $(document).ready(function () {
+        $('.slider').slick({
+            arrows: false, //если не нужны по дизайну
+            dots: true,
+            adaptiveHeight: true,
+            adaptiveWidth: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 5000,
+            responsive: [{
+                breakpoint: 1000,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }, {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    adaptiveHeight: false,
+                    adaptiveWidth: true,
+                }
+            }]
+
+        });
+    })
+})
